06486a95431b21dce3806ca42302ae3e3dfb6337
Andrés Felipe Aguilar Rendón <afaguilarr96@gmail.com>
2018-06-20 11:18:32 -0500

    Validation schema (#16)
    * this is more like an unimportant checkpoint
    
    * this is more like an unimportant checkpoint
    
    * Added the necessary schemas in ListPublicEvents.schema.js and now the validations on GithubApi.Contract.test.js are working
    
    * Solved some issues with step descriptions on the tests

---
+++ test/schema/ListPublicEvents.schema.js
@@ -0,0 +1,121 @@
+const listPublicEventsSchema = {
+  type: 'object',
+  properties: {
+    body: {
+      type: 'array',
+      items: {
+        type: 'object',
+        properties: {
+          id: {
+            type: 'string'
+          },
+          type: {
+            type: 'string'
+          },
+          actor: {
+            type: 'object',
+            properties: {
+              id: {
+                type: 'number'
+              },
+              login: {
+                type: 'string'
+              },
+              display_login: {
+                type: 'string'
+              },
+              gravatar_id: {
+                type: 'string'
+              },
+              url: {
+                type: 'string'
+              },
+              avatar_url: {
+                type: 'string'
+              }
+            }
+          },
+          repo: {
+            type: 'object',
+            properties: {
+              id: {
+                type: 'number'
+              },
+              name: {
+                type: 'string'
+              },
+              url: {
+                type: 'string'
+              }
+            }
+          },
+          payload: {
+            type: 'object',
+            properties: {
+              push_id: {
+                type: 'number'
+              },
+              size: {
+                type: 'number'
+              },
+              distinct_size: {
+                type: 'number'
+              },
+              ref: {
+                type: ['string', 'null']
+              },
+              head: {
+                type: 'string'
+              },
+              before: {
+                type: 'string'
+              },
+              commits: {
+                type: 'array',
+                items: {
+                  properties: {
+                    sha: {
+                      type: 'string'
+                    },
+                    message: {
+                      type: 'string'
+                    },
+                    distinct: {
+                      type: 'boolean'
+                    },
+                    url: {
+                      type: 'string'
+                    },
+                    author: {
+                      type: 'object',
+                      properties: {
+                        email: {
+                          type: 'string'
+                        },
+                        name: {
+                          type: 'string'
+                        }
+                      }
+                    }
+                  }
+                }
+              }
+            },
+            public: {
+              type: 'boolean'
+            },
+            created_at: {
+              type: 'string'
+            }
+          }
+        }
+      }
+    },
+    status: {
+      type: 'number'
+    },
+    required: ['status', 'body']
+  }
+};
+
+exports.listPublicEventsSchema = listPublicEventsSchema;
