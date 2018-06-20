bf636e63c73b7573bcd2be409cd5cc63c157a679
Andrés Felipe Aguilar Rendón <afaguilarr96@gmail.com>
2018-06-20 09:18:29 -0500

    Query parameters (#15)
    * Added queryparameters which limits the number of users returned by the response of the users query
    
    * Hotfix, there was an extra console.log() that wasn't necessary

--- test/GithubApi.Users.test.js
+++ test/GithubApi.Users.test.js
@@ -11,14 +11,23 @@ const expectedTime = 5000;
 
 describe('Response time Api Tests', () => {
   describe('Get All users time response test', () => {
-    let queryResponse;
+    let timeQueryResponse;
     let queryTime;
+    let tenUsersQueryResponse;
+    let fiftyUsersQueryResponse;
+
+    const queryParametersTenUsers = {
+      per_page: 10
+    };
+    const queryParametersFiftyUsers = {
+      per_page: 50
+    };
 
     const callback = (request, time) => {
       queryTime = time;
     };
 
-    function getAllUsersFunction() {
+    function getAllUsersTimeFunction() {
       return agent
         .get(`${apiUrl}/${usersPath}`)
         .auth('token', process.env.ACCESS_TOKEN)
@@ -26,13 +35,33 @@ describe('Response time Api Tests', () => {
         .then(response => response); // Line needed because super-agent-response-time has issues
     }
 
+    function getAllUsersPaginationFunction(queryParameters) {
+      return agent
+        .get(`${apiUrl}/${usersPath}`)
+        .auth('token', process.env.ACCESS_TOKEN)
+        .query(queryParameters)
+        .then(response => response);
+    }
+
     before(async () => {
-      queryResponse = await getAllUsersFunction();
+      timeQueryResponse = await getAllUsersTimeFunction();
+      tenUsersQueryResponse = await
+      getAllUsersPaginationFunction(queryParametersTenUsers);
+      fiftyUsersQueryResponse = await
+      getAllUsersPaginationFunction(queryParametersFiftyUsers);
     });
 
     it('The response should have 200 OK as status and should be less than 5000 ms', () => {
-      expect(queryResponse.status).to.equal(statusCode.OK);
+      expect(timeQueryResponse.status).to.equal(statusCode.OK);
       expect(queryTime).to.be.below(expectedTime);
+    });
+
+    it('The response body should have 10 users', () => {
+      expect(tenUsersQueryResponse.body.length).to.equal(10);
+    });
+
+    it('The response body should have 50 users', () => {
+      expect(fiftyUsersQueryResponse.body.length).to.equal(50);
     });
   });
 });
